3ad3af773e1ef9c2b79a59a80c274c9a5a3352e5
Ubuntu <niel.matthias@gmail.com>
2024-04-25 18:08:31 +0200

    config

--- quartz.config.ts
+++ quartz.config.ts
@@ -68,6 +68,7 @@ const config: QuartzConfig = {
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.HardLineBreaks(),
       Plugin.Description(),
     ],
     filters: [Plugin.RemoveDrafts()],
